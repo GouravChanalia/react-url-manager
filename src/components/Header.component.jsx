1a9882dafc3cd8ed25e2daa1ef8aef2bd0d32d08
Gourav Kumar <gourav.kumar@greenstory.io>
2025-03-29 14:37:04 +0530

    Header changed from URL Manager 3.0 to URL Manager

--- src/components/Header.component.jsx
+++ src/components/Header.component.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="bg-slate-700 p-3 mb-3">
       <div className='text-base font-bold text-white'>Welcome to our little effort to make your life little easier with</div>
-      <div className="text-4xl font-bold text-white">URL Manager 3.0</div>
+      <div className="text-4xl font-bold text-white">URL Manager</div>
     </div>
   )
 }
